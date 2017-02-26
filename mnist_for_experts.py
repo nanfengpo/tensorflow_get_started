a22ed62a142581757c76640779bbec997c55525d
nanfengpo <lsxxxxxx@126.com>
2017-02-26 23:04:15 +0800

    修改了几处错误

--- mnist_for_experts.py
+++ mnist_for_experts.py
@@ -85,10 +85,10 @@ h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob)
 # 输出层(全连接层2):1024->10
 W_fc2=weight_variable([1024,10])
 b_fc2=bias_variable([10])
-y_conv=tf.matmul(h_fc1_drop,W_fc2)+b_fc2
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
 
 # 训练过程
-loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv,y_))
+loss=tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y_conv),reduction_indices=[1]))
 train_step=tf.train.AdamOptimizer(1e-4).minimize(loss)
 correct_prediction=tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
@@ -99,7 +99,7 @@ for i in range(20000):
         train_accuracy=accuracy.eval(feed_dict={x:batch[0],\
                 y_:batch[1],keep_prob:1.0})
         print("step %d, training accuracy %g" %(i,train_accuracy))
-    train_step.run(feed_dictt={x:batch[0],\
+    train_step.run(feed_dict={x:batch[0],\
                 y_:batch[1],keep_prob:0.5})
 print("test accuracy %g" % accuracy.eval(feed_dict={x:batch[0],\
                 y_:batch[1],keep_prob:1.0}))
